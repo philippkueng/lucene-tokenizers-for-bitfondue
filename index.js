8234e28c0b4cd94bfdbd7231c08a43270f5825cb
Philipp Küng <git@philippkueng.ch>
2019-05-20 18:21:54 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const lt = require('lucene-tokenizers');
+
+exports.tokenize = function (text) {
+    var ts = new lt.StandardTokenizer();
+    ts.setReader(new lt.StringReader(text));
+    var res = [], token = null;
+    while ((token = ts.incrementToken()) !== null) {
+      var pretty_token = {};
+      for (var prop in token) {
+        if(token.hasOwnProperty(prop)) {
+          pretty_token[prop.substring('_$esjava$'.length)] = token[prop];
+        } 
+      }
+      res.push(pretty_token);
+    }
+    return res
+}
